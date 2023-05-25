34917f5e7180b7d76ab5551cdcaaaa9b64b50fa4
D3stinn3 <paynedestinne@gmail.com>
2023-05-25 23:09:16 +0300

    Views and Templates

--- src/base/views.py
+++ src/base/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     context = {}
-    return render(request, 'base.html', context)
+    return render(request, 'home.html', context)
